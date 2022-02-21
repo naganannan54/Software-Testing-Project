b685761ea33e53108f543671d21ab7ac104a29e4
Simon Andreasson <sian17@student.bth.se>
2022-02-21 15:19:53 +0100

    Playing around with test cases

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -9,12 +9,13 @@ class CalculatorTest {
     @DisplayName("Add two numbers")
     void add() {
         assertEquals(4, Calculator.add(2,2));
+        assertEquals(13, Calculator.add(2,2,9));
     }
 
     @Test
     @DisplayName("Multiply two numbers")
     void multiply() {
-        assertAll(() -> assertEquals(4, Calculator.multiply(2,2)),
+        assertAll(() -> assertEquals(8, Calculator.multiply(2,2,2)),
                 () -> assertEquals(-4, Calculator.multiply(2, -2)),
                 () -> assertEquals(8, Calculator.multiply(-2,-4)),
                 () -> assertEquals(0, Calculator.multiply(1,0)));
