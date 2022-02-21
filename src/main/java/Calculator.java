5e847c768785a56370ec0d0cb86614345dcff00a
Simon Andreasson <sian17@student.bth.se>
2022-02-21 16:41:35 +0100

    Change calculator functions to public

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -2,11 +2,11 @@ import java.util.stream.DoubleStream;
 
 public class Calculator {
 
-    static double add(double... operands) {
+    public double add(double... operands) {
         return DoubleStream.of(operands).sum();
     }
 
-    static double multiply(double... operands) {
+    public double multiply(double... operands) {
         return DoubleStream.of(operands).reduce(1, (a, b) -> a * b);
     }
 }
