96943ed7631c73804916fcf25d8fb12f2931a744
Simon Andreasson <sian17@student.bth.se>
2022-02-21 15:11:59 +0100

    Testing

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -3,10 +3,10 @@ import java.util.stream.DoubleStream;
 public class Calculator {
 
     static double add(double... operands) {
-        return 0; //DoubleStream.of(operands).sum();
+        return DoubleStream.of(operands).sum();
     }
 
     static double multiply(double... operands) {
-        return 0; //DoubleStream.of(operands).reduce(1, (a, b) -> a * b);
+        return DoubleStream.of(operands).reduce(1, (a, b) -> a * b);
     }
 }
