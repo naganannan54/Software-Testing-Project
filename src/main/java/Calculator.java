e32b0715021bad594e11de625f92d5ebfa824871
Simon Andreasson <sian17@student.bth.se>
2022-02-28 16:56:47 +0100

    Added code

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -4,14 +4,14 @@ public class Calculator {
     }
 
     public int add(int a, int b) {
-        return 0;
+        return a + b;
     }
 
     public int multiply(int a, int b) {
-        return 0;
+        return a * b;
     }
 
-    public int subtract(int a, int b) { return 0; }
+    public int subtract(int a, int b) { return a - b; }
 
-    public int divide(int a, int b) { return 0; }
+    public int divide(int a, int b) { return a / b; }
 }
