90870a5a0f3f0db6387a93918dd4bb16cac0b14f
suvojk <reddysuvoj@gmail.com>
2022-03-12 14:08:57 +0100

    Added integration test cases

--- src/test/java/IntegrationTest.java
+++ src/test/java/IntegrationTest.java
@@ -10,5 +10,11 @@ public class IntegrationTest {
         cal = new Calculator();
 
         assertEquals(8, cal.add(cal.multiply(2,2), cal.divide(8,2)));
+        assertEquals(10, cal.add(cal.multiply(3,2), cal.divide(8,2)));
+        assertEquals(15, cal.add(cal.multiply(5,2), cal.divide(15,3)));
+        assertEquals(2, cal.add(cal.multiply(1,1), cal.divide(2,2)));
+        assertNotEquals(10, cal.add(cal.multiply(3,2), cal.divide(10,2)));
+        assertNotEquals(17, cal.add(cal.multiply(3,4), cal.divide(10,5)));
+        assertNotEquals(28, cal.add(cal.multiply(3,2), cal.divide(18,2)));
     }
 }
